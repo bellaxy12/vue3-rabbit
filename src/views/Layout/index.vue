b7b66af459d66ee9755588ddfb689b6d39ef3607
bellaxy12 <273375387@qq.com>
2023-09-06 17:29:22 +0800

    静态模板搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  我是首页
-  <!-- 二级路由出口 -->
-  <router-view></router-view>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
