2da646df87241dcfb5b8efcf6315187437d70287
bellaxy12 <273375387@qq.com>
2023-09-06 18:10:50 +0800

    吸顶交互实现

--- src/apis/layout.js
+++ src/apis/layout.js
@@ -1,2 +1,3 @@
 import httpInstance from "@/utils/http";
+// 获取layout一级导航列表
 export const getCategoryAPI = () => httpInstance.get("home/category/head");
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,3 @@
 <template>
-  我是home
+  <div style="height: 1200px;">我是home</div>
 </template>
