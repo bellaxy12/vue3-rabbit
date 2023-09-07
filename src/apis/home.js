d077ceb80a85ad1acc720b9a0bd5066f3005dcc4
bellaxy12 <273375387@qq.com>
2023-09-07 17:13:58 +0800

    封装组件实现新鲜好物和热门推荐

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,5 +1,29 @@
 import httpInstance from "@/utils/http";
 
+/**
+ *@Description: 轮播图
+ *@Param: {*}
+ *@Date: 2023-09-07 16:52:35
+*/
 export const getBannerAPI = () => {
   return httpInstance.get('/home/banner')
+}
+
+/**
+ *@Description: 新鲜好物
+ *@Param: {*}
+ *@Date: 2023-09-07 16:44:13
+*/
+export const findNewAPI = () => {
+  return httpInstance.get('/home/new')
+}
+
+/**
+ *@Description: 人气推荐
+ *@Param: {*}
+ *@Date: 2023-09-07 16:52:54
+*/
+
+export const getHotAPI = () => {
+  return  httpInstance.get('home/hot')
 }
